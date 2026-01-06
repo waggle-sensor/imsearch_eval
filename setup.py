98326a4c2ac65d2b75949f7fa15e690cfb74151f
FranciscoLozCoding <francisco.lozano33@outlook.com>
2026-01-06 16:09:24 -0600

    Bump version from 0.1.1 to 0.1.2 in setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ except FileNotFoundError:
 
 setup(
     name="imsearch_eval",
-    version="0.1.1",
+    version="0.1.2",
     description="Abstract benchmarking framework for vector databases and models",
     long_description=long_description,
     long_description_content_type="text/markdown",
