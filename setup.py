51bc2c56e724a56f1690bc1dbcd3c1f9179e4b79
FranciscoLozCoding <francisco.lozano33@outlook.com>
2026-01-08 15:03:47 -0600

    Bump version from 0.1.6 to 0.1.7 in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Adapters are available as optional dependencies.
 
 from setuptools import setup, find_packages
 
-VERSION = "0.1.6"
+VERSION = "0.1.7"
 
 # Core dependencies (always required)
 CORE_DEPS = [
