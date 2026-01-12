9e2139ecb61ae601f428b3731814e1430f6e647e
FranciscoLozCoding <francisco.lozano33@outlook.com>
2026-01-12 16:45:56 -0600

    Update version to 0.1.15 in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Adapters are available as optional dependencies.
 
 from setuptools import setup, find_packages
 
-VERSION = "0.1.14"
+VERSION = "0.1.15"
 
 # Core dependencies (always required)
 CORE_DEPS = [
