d1377ab3d6b15a2f36f920e1ba2b7503d5452c77
FranciscoLozCoding <francisco.lozano33@outlook.com>
2026-01-12 16:35:59 -0600

    Update version to 0.1.13 in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Adapters are available as optional dependencies.
 
 from setuptools import setup, find_packages
 
-VERSION = "0.1.12"
+VERSION = "0.1.13"
 
 # Core dependencies (always required)
 CORE_DEPS = [
