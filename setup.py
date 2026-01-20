6968b4232f1cb093d862d4ea6fd48eeba1097039
FranciscoLozCoding <francisco.lozano33@outlook.com>
2026-01-20 11:50:55 -0600

    updated version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ Adapters are available as optional dependencies.
 
 from setuptools import setup, find_packages
 
-VERSION = "0.1.28"
+VERSION = "0.1.29"
 
 # Core dependencies (always required)
 CORE_DEPS = [
